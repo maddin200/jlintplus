4974074e9f1b2a0de4a895c552d1c90eab897fa8
Martin Strunz <maddin200@aol.com>
2017-04-03 12:35:06 +0200

    Add files via upload

--- jlintwin32/jlint/version.h
+++ jlintwin32/jlint/version.h
@@ -4,10 +4,10 @@
 namespace AutoVersion{
 	
 	//Date Version Types
-	static const char DATE[] = "07";
-	static const char MONTH[] = "02";
+	static const char DATE[] = "03";
+	static const char MONTH[] = "04";
 	static const char YEAR[] = "2017";
-	static const char UBUNTU_VERSION_STYLE[] =  "17.02";
+	static const char UBUNTU_VERSION_STYLE[] =  "17.04";
 	
 	//Software Status
 	static const char STATUS[] =  "Alpha";
@@ -17,13 +17,13 @@ namespace AutoVersion{
 	static const long MAJOR  = 3;
 	static const long MINOR  = 1;
 	static const long BUILD  = 4;
-	static const long REVISION  = 2;
+	static const long REVISION  = 4;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT  = 27;
-	#define RC_FILEVERSION 3,1,4,2
-	#define RC_FILEVERSION_STRING "3, 1, 4, 2\0"
-	static const char FULLVERSION_STRING [] = "3.1.4.2";
+	static const long BUILDS_COUNT  = 46;
+	#define RC_FILEVERSION 3,1,4,4
+	#define RC_FILEVERSION_STRING "3, 1, 4, 4\0"
+	static const char FULLVERSION_STRING [] = "3.1.4.4";
 	
 	//These values are to keep track of your versioning state, don't modify them.
 	static const long BUILD_HISTORY  = 0;
